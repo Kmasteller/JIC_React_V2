8075ca44a35b1edb918be7710744b844b0f21569
kmasteller <kennethmasteller@gmail.com>
2018-03-03 14:03:39 -0700

    carousel working on tour page

--- client/public/assets/js/cloudinary.js
+++ client/public/assets/js/cloudinary.js
@@ -1,5 +1,4 @@
-var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/just-in-case/upload/';
-//w_100,h_190,c_fill,g-auto
+var CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/just-in-case/upload/'
 var CLOUDINARY_UPLOAD_PRESET = 'tsabsfyg';
 
 var imgPreview = document.getElementById('photoPreview');
@@ -7,6 +6,8 @@ var fileUpload = document.getElementById('photo');
 
 fileUpload.addEventListener('change', function (event) {
     var file = event.target.files[0];
+    console.log(file)
+    // CLOUDINARY_URL + file;
     var formData = new FormData();
     formData.append('file', file);
     formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
@@ -20,7 +21,11 @@ fileUpload.addEventListener('change', function (event) {
         data: formData
     }).then(function (res) {
         console.log(res);
-        imgPreview.src = res.data.secure_url;
+  
+        var formatPhotoUrl = `http://res.cloudinary.com/just-in-case/image/upload/w_100,h_190,g_face,c_fill,q_auto/${res.data.public_id}.jpg`
+        console.log(formatPhotoUrl);
+        imgPreview.src = formatPhotoUrl;
+
     }).catch(function (err) {
         console.error(err);
     });
--- client/src/Pages/Tour/Tour.js
+++ client/src/Pages/Tour/Tour.js
@@ -1,52 +1,52 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import {Carousel} from "react-materialize";
+import {Dropdown, Button} from 'react-materialize';
 
 const Tour = (props) => {
   console.log(props)
+
+  const centerAlign = {
+    textAlign: "center"
+  }
+
   return (
     <main>
-      {/* <div>Tour</div> */}
-      {/* Your content here */}
-      <div className="carousel carousel-slider" data-indicators="true">
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item red" to=" " /><img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
-        </div>
-        {/*  */}
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item pink" to=" " /><img src="../../../assets/images/tour/ob2.png" alt="" />
-        </div>
-        {/*  */}
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item purple" to=" " /><img src="../../../assets/images/tour/ob3.png" alt="" />
-        </div>
-        {/*  */}
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item indigo" to=" " /><img src="../../../assets/images/tour/ob4.png" alt="" />
-        </div>
-        {/*  */}
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item blue" to=" " /><img src="../../../assets/images/tour/ob5.png" alt="" />
-        </div>
-        {/*  */}
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item light-blue" to=" " /><img src="../../../assets/images/tour/ob6.png" alt="" />
-        </div>
-        {/*  */}
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item cyan" to=" " /><img src="../../../assets/images/tour/ob7.png" alt="" />
-        </div>
-        {/*  */}
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item teal" to=" " /><img src="../../../assets/images/tour/ob8.png" alt="" />
-        </div>
-        {/*  */}
-        <div className="valign-wrapper center-align">
-          <Link className="carousel-item green" to=" " /><img src="../../../assets/images/tour/ob9.png" alt="" />
-        </div>
-        {/*  */}
+    <Carousel options={{ fullWidth: true}}>
+      <div className="red" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
       </div>
+      <div className="pink" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
+      </div>
+      <div className="purple" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
+      </div>
+      <div className="indigo" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
+      </div>
+      <div className="blue" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
+      </div>
+      <div className="light-blue" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
+      </div>
+      <div className="cyan" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
+      </div>
+      <div className="teal" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
+      </div>
+      <div className="green" style={centerAlign}>
+            <img src="../../../assets/images/tour/ob1.png" alt="" max-width="375px" max-height="432px" />
+      </div>
+
+    </Carousel>
+
+    
+      
       <div className="fixed-action-btn toolbar">
-        <Link className="btn-floating pulse btn-large red" to=" " />
+        <Link className="btn-floating pulse btn-large red" to="/" />
           <i className="large material-icons">menu</i>
         
         <ul>
@@ -56,7 +56,7 @@ const Tour = (props) => {
           <li className="waves-effect waves-light"><Link to="/tour" /><i className="material-icons">play_arrow</i></li>
         </ul>
       </div>
-    </main>
+      </main>
   );
 };
 
