1004097e6f9029141faf4b9dce183f7df3651a0f
kmasteller <kennethmasteller@gmail.com>
2018-03-05 22:10:20 -0700

    resource page udpated, think we are good, can use cards for add too

--- client/src/Pages/Resource/Resource.js
+++ client/src/Pages/Resource/Resource.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { CardTitle, Card, Row, Col } from 'react-materialize';
 import { Link } from "react-router-dom";
-
 
 const Resource = (props) => {
     console.log(props)
+
+    const centerAlign = {
+        textAlign: "center"
+        }
+
+    const fitImage = {
+        // maxWidth: "100%",
+        // maxHeight: "90%",
+        objectFit: "contain"
+        }
+
     return (
         <main>
             {/* Your content here */}
         <div>
             {/* Main Card */}
-            <div className="col s12 s7">
+            <Row>
+            <Col m={7} s={12}>
+            <Card className="card horizontal" style={centerAlign} header={<CardTitle style={fitImage} image= '../../../assets/images/resources/first-aid-kit.jpg' alt='First Aid Kit'></CardTitle>} 
+            actions={[<Link to='https://www.google.com'>Learn More!</Link>]}>
+            <div>
+                <h5 className="resource-title">Click Below to learn more:</h5>
+            </div>
+              <br />
+            </Card>
+            </Col>
+            </Row>
+            {/* <div className="col s12 s7">
                 <div className="card horizontal">
                 <div className="card-image">
                     <img src="./assets/images/resources/first-aid-kit.jpg" alt="First Aid Kit" />
@@ -28,7 +50,109 @@ const Resource = (props) => {
                     </div>
                 </div>
                 </div>
-            </div>
+            </div> */}
+
+        {/* <div class="col s12 s7">
+                <div class="card horizontal">
+                  <div class="card-image">
+                    <img src="./assets/images/resources/first-aid-kit.jpg">
+                  </div>
+                  <div class="card-stacked">
+                    <div class="card-content">
+                        <h5 class="resource-title">First Aid Guidelines</h5>
+                        <br>
+                        <div class="row">
+                                <div class="col s12">
+                                    <img src="./assets/images/resources/ahaarc.png" class="responsive-img">
+                                </div>
+                                <span><b style="font-size:20px"><strong>JUST<font color="red">IN</font>CASE</strong></b> explore first aid and emergency procedures.</span>
+                        </div>
+                    </div>
+                    <div class="card-action">
+                        <p class="black-text text-darken-4"><a href="firstaid.html"><i class="material-icons right">more_horiz</i></a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        <!-- End  -->
+
+        <!-- Second Card -->
+
+        <div class="col s12 s7">
+                <div class="card horizontal">
+                  <div class="card-image">
+                    <img src="./assets/images/resources/poisoncontrol.png">
+                  </div>
+                  <div class="card-stacked">
+                    <div class="card-content">
+                        <h5 class="resource-title">Utah Poison Control</h5>
+                        <br>
+                        <div class="row">
+                                <div class="col s12">
+                                    <img src="./assets/images/resources/uhealth-logo.svg" class="responsive-img">
+                                </div>
+                                <span><b style="font-size:20px"><strong>JUST<font color="red">IN</font>CASE</strong></b> explore first aid and emergency procedures.</span>
+                        </div>
+                    </div>
+                    <div class="card-action">
+                        <p class="black-text text-darken-4"><a href="poisoncontrol.html"><i class="material-icons right">more_horiz</i></a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        <!-- End  -->
+
+        <!-- Third Card -->
+
+        <div class="col s12 s7">
+                <div class="card horizontal">
+                  <div class="card-image">
+                    <img src="./assets/images/resources/fingerprint.gif">
+                  </div>
+                  <div class="card-stacked">
+                    <div class="card-content">
+                        <h5 class="resource-title">F.B.I. recommended<br>Child Safety Kit</h5>
+                        <br>
+                        <div class="row">
+                                <div class="col s12">
+                                    <img src="./assets/images/resources/ncip.png" class="responsive-img">
+                                </div>
+                                <span><b style="font-size:20px"><strong>JUST<font color="red">IN</font>CASE</strong></b> request the child safety kit recommended by the F.B.I. be mailed to you.</span>
+                        </div>
+                    </div>
+                    <div class="card-action">
+                        <p class="black-text text-darken-4"><a href="childsafekit.html"><i class="material-icons right">more_horiz</i></a></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+        <!-- End  -->
+
+        <!-- Fourth Card -->
+
+        <div class="col s12 s7">
+                <div class="card horizontal">
+                  <div class="card-image">
+                    <img src="./assets/images/resources/alert-icon-red.png">
+                  </div>
+                  <div class="card-stacked">
+                    <div class="card-content">
+                        <h5 class="resource-title">Sign Up to receive<br>Local Amber Alerts</h5>
+                        <br>
+                        <div class="row">
+                                <div class="col s12">
+                                    <img src="./assets/images/resources/amber.png" class="responsive-img">
+                                </div>
+                                <span><b style="font-size:20px"><strong>JUST<font color="red">IN</font>CASE</strong></b> you are the able to help a missing or exploited child return home safely.</span>
+                        </div>
+                    </div>
+                    <div class="card-action">
+                        <p class="black-text text-darken-4"><a href="amberalerts.html"><i class="material-icons right">more_horiz</i></a></p>
+                    </div>
+                  </div>
+                </div>
+              </div> */}
+
             {/* End  */}
             <div className="fixed-action-btn toolbar ">
                 <Link className="btn-floating pulse btn-large red" to=""></Link>
